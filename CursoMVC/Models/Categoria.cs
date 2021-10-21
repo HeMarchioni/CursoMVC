804c1817779dbbd2e0849c30dee16c96f3ea8bbf
HeMarchioni <henrique.marchioni@fatec.sp.gov.br>
2021-10-21 00:59:42 -0300

    incluido Validação e corrigindo a View

--- CursoMVC/Models/Categoria.cs
+++ CursoMVC/Models/Categoria.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
 using System.Linq;
 using System.Threading.Tasks;
 
@@ -10,7 +11,9 @@ namespace CursoMVC.Models
     {
 
         public int Id { get; set;}
-
+        
+        [Display(Name = "Descrição")] // -> usando a anotação para definir como ira ser mostrado na tela
+        [Required(ErrorMessage ="O Campo descrição é obrigatorio")]   // -> anotation para falar que o campo é obrigatorio mandar mensagem
         public string Descricao { get; set;}
 
         public List<Produto> Produtos { get; set; }
--- CursoMVC/Models/Produto.cs
+++ CursoMVC/Models/Produto.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
 using System.Linq;
 using System.Threading.Tasks;
 
@@ -10,8 +11,10 @@ namespace CursoMVC.Models
 
         public int Id { get; set; }
 
+        [Display(Name ="Descrição")] // -> usando a anotação para definir como ira ser mostrado na tela
         public string Descricao { get; set; }
 
+        [Range(1,10, ErrorMessage ="Valor fora da Faixa")] // -> anotãção de validação Range -> ja informa direto no display
         public int Quantidade { get; set; }
 
         public int CategoriaId { get; set; }
