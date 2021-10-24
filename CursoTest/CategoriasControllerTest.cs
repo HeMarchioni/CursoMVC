4cc24981471f756a6a9102910a8eb3a4613b22ff
HeMarchioni <henrique.marchioni@fatec.sp.gov.br>
2021-10-24 18:53:47 -0300

    implementação test

---
+++ CursoTest/CategoriasControllerTest.cs
@@ -0,0 +1,44 @@
+﻿using CursoAPI.Controllers;
+using CursoMVC.Models;
+using Microsoft.EntityFrameworkCore;
+using Moq;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Xunit;
+
+namespace CursoTest
+{
+    public class CategoriasControllerTest
+    {
+
+        private readonly Mock<DbSet<Categoria>> mockSet;
+        private readonly Mock<Context> mockContext;
+        private readonly Categoria categoria;
+
+
+        public CategoriasControllerTest()
+        {
+
+            mockSet = new Mock<DbSet<Categoria>>();
+            mockContext = new Mock<Context>();
+            categoria = new Categoria { Id = 1, Descricao = "Teste Categoria" };
+        
+        }
+
+        [Fact]
+        public async Task Get_Categoria()
+        {
+            var service = new CategoriasController(mockContext.Object); // -> instancio o controller que vou usar (passo o context)
+
+            await service.GetCategoria(1);  //- > agora tenho acesso a seus metodos 
+
+            mockSet.Verify(m => m.FindAsync(1), Times.Once());
+
+        }
+
+
+    }
+}
